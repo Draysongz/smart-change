58937b31381fb7ff2a57a36245fba5756cc5a3a3
Ayomide Gidigbi <70198955+Draysongz@users.noreply.github.com>
2022-09-28 15:45:48 +0100

    new

--- src/Pages/CryptoStats.jsx
+++ src/Pages/CryptoStats.jsx
@@ -3,12 +3,16 @@ import { Row } from 'react-bootstrap'
 import { Col } from 'react-bootstrap'
 import TrendingCoin from '../components/TrendingCoin'
 import useAxios from '../Hooks/useAxios'
-
+import {Slide} from 'react-awesome-reveal'
+import trending from '../assets/trending.png'
 import './CryptoStats.css'
 function CryptoStats() {
  const {response}= useAxios("search/trending")
   return (
     <div>
+        <div className="left">
+            <img src={trending} alt="trending" />
+        </div>
         <div className="column-container">
             <h3>Trending</h3>
             <Row className='rows'>
@@ -16,7 +20,9 @@ function CryptoStats() {
                 <Col >Price/BTC</Col>
                 <Col>ID</Col>
             </Row>
+            <Slide direction='up'>
             {response && response.coins.map(coin=> <TrendingCoin key={coin.item.coin_id} coin = {coin.item}/>  )}
+            </Slide>
          </div>
     </div>
   )
