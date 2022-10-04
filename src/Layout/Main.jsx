160843f1c6aac4c1bca0e9de61b876d893536dc9
Ayomide Gidigbi <70198955+Draysongz@users.noreply.github.com>
2022-10-04 17:53:51 +0100

    updated

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from '../Pages/Hero'
+import { useState } from 'react'
 import CryptoStats from '../Pages/CryptoStats'
 import Features from '../Pages/Features'
+import Countdown from '../Pages/Countdown'
 
 
 const Main = () => {
+  const [timerDays, setTimerDays]= useState()
+  const [timerHours, setTimerHours]= useState()
+  const [timerMinutes, setTimerMinutes]= useState()
+  const [timerSeconds, setTimerSeconds]= useState()
+
+  let interval;
+
+  const startTimer = ()=>{
+    const countDownDate= new Date("November 4, 2022").getTime();
+
+    interval= setInterval(()=>{
+      const currentDate= new Date().getTime();
+
+    const difference= countDownDate - currentDate
+
+    const days = Math.floor(difference/(24*60*60*1000))
+
+    const hours = Math.floor(difference%(24*60*60*1000)/(1000*60*60))
+
+    const minutes = Math.floor(difference%(60*60*1000)/(1000*60))
+
+    const seconds = Math.floor(difference%(60*1000)/(1000))
+
+    if(difference<0){
+      //stop
+
+      clearInterval(interval.current)
+    }else{
+      //Update Timer
+
+      setTimerDays(days)
+      setTimerHours(hours)
+      setTimerMinutes(minutes)
+      setTimerSeconds(seconds)
+    }
+    })
+  }
+
+  useEffect(()=>{
+    startTimer()
+  })
+
   return (
     <div>
       <Hero/>
       <CryptoStats/>
       <Features/>
+      <Countdown timerDays={timerDays} timerHours={timerHours} timerMinutes={timerMinutes} timerSecoonds={timerSeconds}/>
     </div>
   )
 }
--- src/Pages/Countdown.jsx
+++ src/Pages/Countdown.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import './Countdown.css'
+import { useState, useEffect, Fragment } from 'react'
 
-const Countdown = () => {
+const Countdown = ({timerDays, timerHours, timerMinutes, timerSeconds}) => {
+   
   return (
-    <div>Countdown</div>
+    <Fragment>
+        <section className="timer-container">
+            <section className="timer">
+
+                <div className="clock">
+                    <section>
+                        <p>{timerDays}</p>
+                        <small>Days</small>
+                    </section>
+                    <span>:</span>
+
+                    <section>
+                        <p>{timerHours}</p>
+                        <small>Hours</small>
+                    </section>
+                    <span>:</span>
+
+                    <section>
+                        <p>{timerMinutes}</p>
+                        <small>Minutes</small>
+                    </section>
+                    <span>:</span>
+
+                    <section>
+                        <p>{timerSeconds}</p>
+                        <small>Seconds</small>
+                    </section>
+                </div>
+            </section>
+        </section>
+    </Fragment>
   )
 }
 
+Countdown.defaultProps={
+    timerDays:10,
+    timerHours:10,
+    timerMinutes:10,
+    timerSeconds:10
+}
 export default Countdown
