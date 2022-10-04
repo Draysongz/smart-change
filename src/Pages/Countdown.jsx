252ab091fc489d3cd5b58f74488485bf735a904c
Ayomide Gidigbi <70198955+Draysongz@users.noreply.github.com>
2022-10-04 17:06:09 +0100

    updated

---
+++ src/Pages/Countdown.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const Countdown = () => {
+  return (
+    <div>Countdown</div>
+  )
+}
+
+export default Countdown
--- src/Pages/Features.jsx
+++ src/Pages/Features.jsx
@@ -1,36 +1,46 @@
 import React from 'react'
-import iphone from '../assets/iphone.png'
-import eclipse from '../assets/eclipse.png'
-import {BsFillCheckCircleFill} from 'react-icons/bs'
-import {Slide} from 'react-awesome-reveal'
-import {Zoom} from 'react-awesome-reveal'
 import './Features.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoneyBill1Wave } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import {faCartShopping} from '@fortawesome/free-solid-svg-icons'
 
 const Features = () => {
   return (
-<div className="features-container"> 
-    <div className="features">
-        <div className="mockup-caption">
-            <Zoom>
-            <h1>Features</h1>
-            </Zoom>
-            <Slide direction='top' cascade damping={0.3}>
-            <h3 className='Ui'> <BsFillCheckCircleFill className='check'/> User Friendly UI</h3>
-            <p className='desc'>Smart change has been built with an easy and simple UI</p>
-            <h3 className='Ui'> <BsFillCheckCircleFill className='check'/> Easy and Fast B2B swap</h3>
-            <p className='desc'>Swap your coins easily with our B2B swap feature</p>
-            <h3 className='Ui'> <BsFillCheckCircleFill className='check'/> Trade with Verified Merchants</h3>
-            </Slide>
+    <div className="features-container">
+      <h1 className="feature-title">Features</h1>
+        {/*features div container */}
+      <div className="features">
+          {/*first div */}
+        <div className="buy">
+          <div className="buy-icon">
+          <FontAwesomeIcon icon={faMoneyBill1Wave} className='font' />
+          </div>
+        <h2 className='buy-title'>Buy</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur<br />
+         Molestias cum unde voluptatem aut quas sapiente non? Et aut sapiente maiores corporis!</p>
         </div>
-        <Zoom>
-        <div className="first-mockup">
-       
-            <img src={eclipse} alt="" className="eclipse" width='500' />
-            <img src={iphone} alt="merchant" className="iphone" width='550'  />
+
+        {/*Second div */}
+        <div className="sell">
+        <div className="buy-icon">
+          <FontAwesomeIcon icon={faCartShopping} className='font' />
+          </div>
+        <h2 className='sell-title'>Sell</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur<br />
+         Molestias cum unde voluptatem aut quas<br />
+         sapiente non? Et aut sapiente maiores corporis!</p>
         </div>
-        </Zoom>
+        <div className="swap">
+        <div className="buy-icon">
+          <FontAwesomeIcon icon={faArrowRightArrowLeft} className='font' />
+          </div>
+        <h2 className='swap-title'>B2B Swap</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur<br />
+         Molestias cum unde voluptatem aut quas sapiente non? Et aut sapiente maiores corporis!</p>
+        </div>
+      </div>
     </div>
-</div>
   )
 }
 
