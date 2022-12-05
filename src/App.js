e7affb98f916869c0237aa9f1ec1e65a4a755330
Ackermann <dharmiegra4@gmail.com>
2022-12-05 21:43:20 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Main from "./Layout/Main";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from 'react-toastify'
 
 function App() {
   return (
+    
     <div className="App">
+    
       <Main/>
+      <ToastContainer/>
     </div>
+    
   );
 }
 
--- src/Pages/Waitlist.jsx
+++ src/Pages/Waitlist.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import './Waitlist.css'
+import {db} from '../firebase'
+import { addDoc, collection } from 'firebase/firestore'
+import { toast } from 'react-toastify'
+import { useState } from 'react'
 
 const Waitlist = () => {
+const [email, setEmail] = useState('')
+
+const onSubmit= async (e)=>{
+  e.preventDefault()
+  try {
+    await addDoc(collection(db, "emails"), {
+      email: email
+    });
+    toast('Sucessfully Registered')
+  } catch (error) {
+    toast.error('An error occurred')
+    console.log(error)
+  }
+}
 
   return (
     <div>
         <div className="list-container">
           <h1>Join The Waitlist</h1>
           <div className="waitlist-input">
-            <input type="email"  placeholder='johndoe@gmail.com' id='email' className="list-input" />
-            <button className="input-btn">Submit</button>
+            <input type="email"  placeholder='johndoe@gmail.com' id='email' onChange={e=>setEmail(e.target.value)} required  className="list-input" />
+            <button onClick={onSubmit}  className="input-btn">Submit</button>
           </div>
       
         </div>
